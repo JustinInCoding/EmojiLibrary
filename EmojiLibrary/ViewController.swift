43faba0c310c69762fdfd566242a3741215c9725
JustinSheng <justin.wang4@icloud.com>
2024-03-28 16:57:57 +0800

    Adding DataSource Methods

--- EmojiLibrary/ViewController.swift
+++ EmojiLibrary/ViewController.swift
@@ -38,9 +38,11 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var collectionView: UICollectionView!
+	let dataSource = DataSource()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+		collectionView.dataSource = dataSource
   }
 }
 
